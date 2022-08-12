fa4457fd9f14149f99cec77809affc9ddab957ed
ashish533 <108485504+ashish533@users.noreply.github.com>
2022-08-12 18:58:23 +0530

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,13 @@
+""" Datatypes in Python """
+''' today is monday   '''
+
+# print(type('hello'))
+# print(type("hey"))
+# print(''' hello 
+#           python''')
+# a='''python 
+
+# coding'''
+# print(a)
+
+'''Note: Just shown int , float and str datatype'''
